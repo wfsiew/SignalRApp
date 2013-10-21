102f09f16e87ee0210f9f7e2d2262d27efd4b647
wfsiew <siewwingfei@hotmail.com>
2013-10-22 00:28:12 +0800

    first commit

---
+++ SignalRApp/Models/ChatRooms.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Web;
+
+namespace SignalRApp.Models
+{
+    public class ChatRooms
+    {
+        static ConcurrentBag<string> rooms = new ConcurrentBag<string>();
+
+        static ChatRooms()
+        {
+            rooms.Add("Lobby");
+        }
+
+        public static void Add(string name)
+        {
+            rooms.Add(name);
+        }
+
+        public static bool Exists(string name)
+        {
+            return rooms.Contains(name);
+        }
+
+        public static IEnumerable<string> GetAll()
+        {
+            return rooms;
+        }
+    }
+}
---
+++ SignalRApp/MyHub.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using SignalRApp.Models;
+
+namespace SignalRApp
+{
+    public class MyHub : Hub
+    {
+        public void CreateChatRoom(string room)
+        {
+            if (!ChatRooms.Exists(room))
+            {
+                ChatRooms.Add(room);
+                Clients.All.addChatRoom(room);
+            }
+        }
+
+        public void Join(string room)
+        {
+            Groups.Add(Context.ConnectionId, room);
+        }
+
+        public void Send(string room, string message)
+        {
+            Clients.Group(room).addMessage(room, message);
+        }
+
+        public override Task OnConnected()
+        {
+            foreach (string room in ChatRooms.GetAll())
+                Clients.Caller.addChatRoom(room);
+
+            return base.OnConnected();
+        }
+    }
+}
